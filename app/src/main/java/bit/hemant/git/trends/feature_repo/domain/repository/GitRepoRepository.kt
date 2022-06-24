b5287f84157ea89fadc4122fbb39b6e4687d415a
hemantsb <hemant.bisht@outlook.com>
2022-06-25 00:34:36 +0530

    repo added

--- app/src/main/java/bit/hemant/git/trends/feature_repo/domain/repository/GitRepoRepository.kt
+++ app/src/main/java/bit/hemant/git/trends/feature_repo/domain/repository/GitRepoRepository.kt
@@ -1,6 +1,11 @@
 package bit.hemant.git.trends.feature_repo.domain.repository
 
+import bit.hemant.git.trends.feature_repo.domain.model.Repo
+import kotlinx.coroutines.flow.Flow
+
 interface GitRepoRepository {
 
+    fun getRepoList(): Flow<List<Repo>>
 
+    suspend fun insertRepos(repos: List<Repo>)
 }
